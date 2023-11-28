bf4415abe0f58f9ff8a47627b15f238f1bec7bf9
hcpinho123 <147578581+hcpinho123@users.noreply.github.com>
2023-11-28 08:53:15 -0500

    Add files via upload

---
+++ shoppinglist.py
@@ -0,0 +1,71 @@
+'''Henrique Pinho
+Date I did it: 11/09/2023
+sources: https://doc.qt.io/qtforpython-6/PySide6/QtWidgets/QListWidget.html#
+'''
+
+import os 
+
+from PySide6.QtWidgets import \
+     QApplication, QMainWindow, QVBoxLayout, QWidget, \
+     QLabel, QLineEdit, QPushButton, QListWidget, QAbstractItemView
+
+
+class shoppingList(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Shopping List!")
+        layout = QVBoxLayout()
+        
+        layout.addWidget(QLabel("Shopping List"))
+        
+        self.item = QLineEdit()
+        self.item.setPlaceholderText("item")
+        layout.addWidget(self.item)
+        
+        addButton = QPushButton("Add!")
+        addButton.clicked.connect(self.additem)
+        self.item.returnPressed.connect(self.additem)
+        layout.addWidget(addButton)
+        
+        removeButton = QPushButton("Remove!")
+        removeButton.clicked.connect(self.removeitem)
+        layout.addWidget(removeButton)
+        
+        self.shopping_list = QListWidget(self)
+        self.shopping_list.setSortingEnabled(True)
+        
+        path = 'myfile.txt'
+        if os.path.exists(path):
+            f = open("myfile.txt", "r")
+            for i in f.readlines():
+                self.shopping_list.addItem(i)
+                f.close()
+        
+        self.shopping_list.setSelectionMode(QAbstractItemView.MultiSelection)
+        layout.addWidget(self.shopping_list)
+
+        
+        # add layout to window
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        
+    def additem(self):
+        self.shopping_list.addItem(self.item.text())
+        f = open("myfile.txt", "a")
+        f.write(self.item.text())
+        f.write('\n')
+        f.close()
+        self.item.setText("")
+                    
+    def removeitem(self):
+        for item in self.shopping_list.selectedItems():
+            self.shopping_list.row(item)
+            self.shopping_list.takeItem(self.shopping_list.row(item))
+        
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = shoppingList()
+    window.show()
+    app.exec()
